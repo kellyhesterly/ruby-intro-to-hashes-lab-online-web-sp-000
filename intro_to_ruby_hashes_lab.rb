f0839d6f65531653ba0a550d4b305feeb79f6d14
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-02 21:06:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,6 @@ def monopoly_with_third_tier
   monopoly = {
     railroads: {pieces: 4, names: {}, rent_in_dollars:
       {one_piece_owned: 25,
-       two_piece_owned: 50}},
+       two_pieces_owned: 50}},
   }
 end
